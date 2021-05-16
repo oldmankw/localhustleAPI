457bc1fdb9f82b68da1a2303fd7ab0ac50994464
photo <photo@photon>
2021-05-16 20:00:18 +0200

    Re arranging the application

---
+++ localhustleZA/src/main/java/com/localhustle/api/localhustle/controller/HustleController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.localhustle.api.localhustle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author photo
+ */
+@Controller
+@RequestMapping("/api")
+public class HustleController 
+{
+    @GetMapping("/hustle")
+    public String getHello()
+    {
+        return "Heeeeeeeeeeeeeeeeeee Yooooouuuuuuuuuuuuuu";
+    }
+}
